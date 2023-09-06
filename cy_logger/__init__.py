307d448782adea87818ffa757ca8bd6fbbb0a880
nttlong <zugeliang2000@gmail.com>
2023-09-06 13:55:10 +0700

    test

--- cy_logger/__init__.py
+++ cy_logger/__init__.py
@@ -2,11 +2,12 @@ import os
 import pathlib
 import logging
 
+
 class Logger:
     def __init__(self):
         self.__working_dir__ = pathlib.Path(__file__).parent.parent.__str__()
         self.__log_path__ = os.path.abspath(
-            os.path.join(self.__working_dir__,"share-storage","logger","logs.log")
+            os.path.join(self.__working_dir__, "share-storage", "logger", "logs.log")
         )
         self.__logger__ = logging.basicConfig(
             format="%(asctime)s %(levelname)-8s %(message)s",
@@ -14,5 +15,3 @@ class Logger:
             filename=self.__log_path__,
             filemode="w",
         )
-
-
