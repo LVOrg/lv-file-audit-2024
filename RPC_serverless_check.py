4723fddcc5d6a614733467fcdc2f73c15992ea26
nttlong <zugeliang2000@gmail.com>
2023-10-26 16:48:32 +0700

    testtest

---
+++ RPC_serverless_check.py
@@ -0,0 +1,18 @@
+import pathlib
+import sys
+
+working_dir = pathlib.Path(__file__).parent.__str__()
+sys.path.append(working_dir)
+sys.path.append("/app")
+from cy_grpc.serverless import start_serverles
+def handler(data:dict,context)->dict:
+    try:
+        data["test"] =  1
+        return dict(
+            data=data
+        )
+    except Exception as e:
+        return  dict(
+            error=str(e)
+        )
+start_serverles(50011,handler)
