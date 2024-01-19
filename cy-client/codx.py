1ddda4510d37317bced61c535990bdf367c3a7f8
nttlong <zugeliang2000@gmail.com>
2024-01-19 12:24:17 +0700

    testtest

--- cy-client/codx.py
+++ cy-client/codx.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
-# build  pyinstaller --onefile E:\long\python\main.py --icon=E:\long\python\icon\WP.ico --noconsole
+# how to build
+# pyinstaller --onefile E:\long\python\codx.py --icon=E:\long\python\icon\WP.ico --noconsole
 import pathlib
 import threading
 import time
@@ -39,6 +40,7 @@ def download(url):
     with open(download_path, "wb") as f:
         f.write(response.content)
     return download_path
+stop_server = False
 async def hello_world(websocket, path:str):
     url=path.split("=")[1]
     doc_path=download(url)
@@ -47,6 +49,7 @@ async def hello_world(websocket, path:str):
     word.Visible = True  # Make Word window visible
 
     doc = word.Documents.Open(doc_path)
+    word.Activate()  # This brings Word to the foreground
 
     await websocket.send("helloworld")
 
@@ -55,39 +58,72 @@ from PIL import Image
 import sys
 verion = 'p.0.1'
 app_dir=pathlib.Path(__file__).parent.__str__()
-icon_extractor = extract_icon.ExtractIcon(sys.executable)
-raw = icon_extractor.get_raw_windows_preferred_icon()
-icon_io = io.BytesIO(raw)
-app_icon = Image.open(icon_io)
+def get_icon():
+    icon_extractor = extract_icon.ExtractIcon(sys.executable)
+    raw = icon_extractor.get_raw_windows_preferred_icon()
+    icon_io = io.BytesIO(raw)
+    app_icon = Image.open(icon_io)
+    return app_icon
+server=None
+start_server= None
 
+async def handler(websocket):
+    while not stop_server:
+        await websocket.recv()
+
+
+async def server_async():
+    async with websockets.serve(hello_world, "localhost", 8765):
+        await asyncio.Future()  # Run forever until stopped
+
+
+def start_socket():
+    asyncio.run(server_async())
 def create_tray_icon(icon):
     def show_window():
         # Code to display your application's main window
         pass
     def exit_app():
-        icon.stop()  # Stop the tray icon
-        sys.exit()  # Exit the application
+        try:
+            # start_server.ws_server.server.close()
+            icon.stop()  # Stop the tray icon
+            sys.exit()  # Exit the application
+        except Exception as e:
+            print(e)
+            pass
+        finally:
+            sys.exit(0)
      # Load your icon
     menu = pystray.Menu(
         pystray.MenuItem("Show Window", show_window),
         pystray.MenuItem("Exit", exit_app),
     )
     icon = pystray.Icon("Codx Office interact", icon, menu=menu)
-    threading.Thread(target=icon.run).start()
-    start_server = websockets.serve(hello_world, "localhost", 8765)
-
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
-    return icon
+    return icon.run
 
 
 try:
 # In your main script, call this function to create the tray icon
-    create_tray_icon(app_icon)
+    app_icon= get_icon()
+    tray_icon_run=create_tray_icon(app_icon)
+    th1=threading.Thread(target=start_socket)
+    th1.name="Socket"
+    th2=threading.Thread(target=tray_icon_run)
+    th2.name="Tray"
+    th2.start()
+    # th1.start()
+
+
+    # th1.join()
+    start_socket()
+    th2.join()
+    # threading.Thread(target=tray_icon_run).start()
+    #
+    # start_socket()
 except Exception as e:
     txt = traceback.print_exc()
     print(__file__)
     print(txt)
-    time.sleep(10000)
+    # time.sleep(10000)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
