9dd56bf489f90a62655cb9e71852ff3c736229b6
nttlong <zugeliang2000@gmail.com>
2024-02-26 09:26:47 +0700

    testtest

---
+++ cy_ai_cloud/check_001.py
@@ -0,0 +1,15 @@
+import pathlib
+import sys
+sys.path.append(pathlib.Path(__file__).parent.parent.__str__())
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
