9bab3435b94d57daf34f6fdf2db361f9fdeede93
nttlong <zugeliang2000@gmail.com>
2024-03-13 11:08:39 +0700

    testtesttesttest

--- cy_xdoc/server.py
+++ cy_xdoc/server.py
@@ -15,7 +15,7 @@ import cyx.framewwork_configs
 import cyx.common
 from cyx.common import config
 # if hasattr(config,"file_storage_encrypt") and config.file_storage_encrypt==True:
-import cy_file_cryptor
+import cy_file_cryptor.wrappers
 import cy_file_cryptor.settings
 
 cy_file_cryptor.settings.set_encrypt_folder_path(config.file_storage_path)
