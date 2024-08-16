ccd0354796011b26192c774f806df5396b51926a
nttlong <zugeliang2000@gmail.com>
2024-08-16 14:01:19 +0700

    remove all big files

--- cyx/malloc_services.py
+++ cyx/malloc_services.py
@@ -1,14 +1,16 @@
 import gc
 import ctypes
 import asyncio
+import sys
 import threading
 
 
 class MallocService:
     def reduce_memory(self):
         gc.collect()
-        libc = ctypes.CDLL("libc.so.6")
-        libc.malloc_trim(0)
+        if not sys.platform in ["win32","win64"]:
+            libc = ctypes.CDLL("libc.so.6")
+            libc.malloc_trim(0)
 
     async def async_reduce_memory(self):
         await asyncio.to_thread(lambda: gc.collect())  # Collect garbage asynchronously
